f07a58c3fd9a6abec891715576260bf136cf1a91
elenacuoco <elena.cuoco@gmail.com>
2014-12-30 18:31:48 +0100

    Delete London-Scikit-Learn.py

--- London-Scikit-Learn.py
+++
@@ -1,8 +0,0 @@
-print(__doc__)
-'''london-scikit competions'''
-'''author Elena Cuoco'''
-import numpy as np
-import pandas as pd
-import os
-import csv
-from sklearn.cross_validation import cross_val_score
