02d69533fc9c5adb607d2125abcbffb535b7e90d
elenacuoco <elena.cuoco@gmail.com>
2014-12-30 11:24:25 +0100

    Create London-Scikit-Learn.py
    Leaderboard code

---
+++ London-Scikit-Learn.py
@@ -0,0 +1,8 @@
+print(__doc__)
+'''london-scikit competions'''
+'''author Elena Cuoco'''
+import numpy as np
+import pandas as pd
+import os
+import csv
+from sklearn.cross_validation import cross_val_score
